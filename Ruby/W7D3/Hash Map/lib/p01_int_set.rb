c1ef18dfdc813e43db7773b0998aa2d5fe84dc8c
Chas Huggins <hugginsc10@gmail.com>
2019-07-10 15:55:17 -0400

    p01 final commit

--- Ruby/W7D3/Hash Map/lib/p01_int_set.rb
+++ Ruby/W7D3/Hash Map/lib/p01_int_set.rb
@@ -1,22 +1,36 @@
 class MaxIntSet
+  attr_reader :store
   def initialize(max)
+   @store = Array.new(max, false)
+   
   end
 
   def insert(num)
+    validate!(num)
+    return false if @store[num]
+    @store[num] = true
+    
   end
 
   def remove(num)
+    validate!(num)
+    return nil unless include?(num)
+    @store[num] = false
   end
 
   def include?(num)
+    validate!(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    (0...@store.length).include?(num)
   end
 
   def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
   end
 end
 
@@ -27,18 +41,23 @@ class IntSet
   end
 
   def insert(num)
+    return false if include?(num)
+    self[num] << num
   end
 
+
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -50,23 +69,33 @@ class ResizingIntSet
   attr_reader :count
 
   def initialize(num_buckets = 20)
+    @num_buckets = num_buckets
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
   def insert(num)
+    return false if include?(num)
+    self[num] << num
+    @count += 1
+    resize! if num_buckets < @count
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +103,10 @@ class ResizingIntSet
   end
 
   def resize!
+    store = @store
+    @num_buckets *= 2
+    @count = 0
+    @store = Array.new(@num_buckets) {Array.new}
+    store.flatten.each {|num| insert(num)}
   end
 end
