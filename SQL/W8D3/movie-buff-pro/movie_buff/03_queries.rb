6b327dcff588852ab1e4df3c15415d1b96145f3b
Chas Huggins <hugginsc10@gmail.com>
2019-07-17 16:38:42 -0400

    final upload for movie_buff

--- SQL/W8D3/movie-buff-pro/movie_buff/03_queries.rb
+++ SQL/W8D3/movie-buff-pro/movie_buff/03_queries.rb
@@ -1,24 +1,47 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
-
+  Movie
+    .select(:title, :id)
+    .joins(:actors)
+    .where(actors: {name: those_actors})
+    .group(:id)
+    .having('COUNT(actors.id) >= ?', those_actors.length)
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
-
+  Movie
+    .select('AVG(score), (yr/10) * 10 as decade')
+    .group('decade')
+    .order('AVG(score) DESC')
+    .first
+    .decade
 end
 
 def costars(name)
   # List the names of the actors that the named actor has ever
   # appeared with.
   # Hint: use a subquery
+  subquery = Movie.select(:id).joins(:actors).where(actors: {name: name})
+
+  Movie
+    .joins(:actors)
+    .where(movies: {id: subquery})
+    .where.not(actors: {name: name})
+    .distinct
+    .pluck(:name)
+    
 
 end
 
 def actor_out_of_work
   # Find the number of actors in the database who have not appeared in a movie
-
+  Actor
+    .select(:name)
+    .joins('LEFT OUTER JOIN castings ON actors.id = castings.actor_id')
+    .where(castings: {movie_id: nil})
+    .count
 end
 
 def starring(whazzername)
@@ -28,7 +51,10 @@ def starring(whazzername)
 
   # ex. "Sylvester Stallone" is like "sylvester" and "lester stone" but
   # not like "stallone sylvester" or "zylvester ztallone"
-
+  name = "%#{whazzername.split(//).join('%')}%"  #must use double parenthesis not single
+  Movie
+    .joins(:actors)
+    .where('UPPER(actors.name) LIKE UPPER(?)', name)
 end
 
 def longest_career
@@ -36,5 +62,10 @@ def longest_career
   # (the greatest time between first and last movie).
   # Order by actor names. Show each actor's id, name, and the length of
   # their career.
-
+  Actor
+    .select(:name, :id, 'MAX(movies.yr) - MIN(movies.yr) as career')
+    .joins(:movies)
+    .group(:id)
+    .order('career DESC, name')
+    .limit(3)
 end
